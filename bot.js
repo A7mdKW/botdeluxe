fa7dfc5ca5d9f2e41afebccce1ce13f51e903769
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2018-12-15 17:14:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -588,7 +588,7 @@ if (message.member.voiceChannel == null) return message.channel.send(`**الرج
 
 
 client.on("guildMemberAdd", member => {
-let welcomer = member.guild.channels.find("name","swalf-سوالف");
+let welcomer = member.guild.channels.find("name","log");
       if(!welcomer) return;
       if(welcomer) {
          moment.locale('ar-ly');
@@ -606,47 +606,7 @@ let welcomer = member.guild.channels.find("name","swalf-سوالف");
       }
       });
 
-/*var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "7Disaster")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'swalf-سوالف');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "7Disaster")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` **تم دعوته من قبل :** : ${Invite.inviter}  `)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});*/
+
 client.on('guildMemberAdd',async member => {
   const Canvas = require('canvas');
   const jimp = require('jimp');
