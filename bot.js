cd05430e2bb511a37da03b5e5c3e99fd05f3328d
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2019-05-12 01:36:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -299,8 +299,13 @@ client.on('message', message => {
     }
 });
 client.on('message', message => {
-    if(message.content === 'فليحان'){
-        message.reply('**خرا فيه **');
+    if(message.content === 'تسجيل دخول '){
+        message.reply('** ✅ تم تسجيل دخولك**');
+    }
+});
+client.on('message', message => {
+    if(message.content === 'تسجيل خروج'){
+        message.reply('**✅ تم تسجيل خروجك  **');
     }
 });
  client.on("guildBanAdd", (guild, member) => {
@@ -606,68 +611,30 @@ let welcomer = member.guild.channels.find("name","log");
       }
       });
 
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '💖الترحيب');
+    let memberavatar = member.user.avatarURL
+	let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('#36393e')
+        .setThumbnail(memberavatar)
+		.setImage('https://cdn.discordapp.com/attachments/369883019055726592/576892746539860029/123123.png')
+        .addField('💬 | Name :  ',`${member}`)
+        .addField('📢 | نورت السيرفر' , `WelCome To Server, ${member}`)
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("حياك الله ! :revolving_hearts: ",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' Server:', `${member.guild.name}`,true)
+                                       
+     .setFooter(`DV : NayerAZ , Ahmad KW`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
 
-client.on('guildMemberAdd',async member => {
-  const Canvas = require('canvas');
-  const jimp = require('jimp');
-  const w = ['./img/welcome_2.png'];
-        let Image = Canvas.Image,
-            canvas = new Canvas(800, 300),
-            ctx = canvas.getContext('2d');
-        ctx.patternQuality = 'bilinear';
-        ctx.filter = 'bilinear';
-        ctx.antialias = 'subpixel';
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-        ctx.shadowOffsetY = 2;
-        ctx.shadowBlur = 2;
-        ctx.stroke();
-        ctx.beginPath();
- 
-        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-            if (err) return console.log(err);
-            let BG = Canvas.Image;
-            let ground = new Image;
-            ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 800, 300);
- 
-})
- 
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                 if (err) return console.log(err);
- 
-          ctx.font = '36px Arial';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(member.user.username, 545, 177);
-         
-          ctx.font = '16px Arial Bold';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
-         
-          let Avatar = Canvas.Image;
-          let ava = new Avatar;
-          ava.src = buf;
-          ctx.beginPath();
-          ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
-          ctx.closePath();
-          ctx.clip();
-          ctx.drawImage(ava, 36, 21, 260, 260);
-          
-                      ctx.font = '20px Arial';
-                        ctx.fontSize = '28px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-ctx.fillText(`Welcome To .....`
-                        , 120, 20);
-                        
- client.channels.get("575776006066143234").sendFile(canvas.toBuffer())
-          client.channels.get("575776006066143234").send(`**Welcome** ${member} To **Deluxe Server**`)
+
            
           
        
